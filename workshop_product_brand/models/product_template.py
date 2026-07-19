4f764e7b06dd8e73bd549816f62e4910d3ebd9d4
phyoeminko <devphyoeminko@gmail.com>
2026-07-19 22:03:39 +0630

    [ADD] myanmar name in product

--- workshop_product_brand/models/product_template.py
+++ workshop_product_brand/models/product_template.py
@@ -4,6 +4,10 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    english_name = fields.Char(
+        string="English Name",
+        index=True,
+    )
     brand_id = fields.Many2one(
         "workshop.product.brand",
         string="Brand",
