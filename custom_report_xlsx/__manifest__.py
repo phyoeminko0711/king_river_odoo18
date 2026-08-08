7b952eeec9f4a36c56f22f5ab554fd4da73840cc
phyoeminko <devphyoeminko@gmail.com>
2026-08-08 09:26:54 +0630

    [UPDATE] sale detail and analysis report

--- custom_report_xlsx/__manifest__.py
+++ custom_report_xlsx/__manifest__.py
@@ -25,9 +25,9 @@
 
     'data': [
         'security/ir.model.access.csv',
-        # 'wizards/sale_detail_report.xml',
+        'wizards/sale_detail_report.xml',
         # 'wizards/purchase_detail_report.xml',
-        # 'wizards/sale_analysis_detail_report_wizard.xml',
+        'wizards/sale_analysis_detail_report_wizard.xml',
         'wizards/daily_sales_summary_report.xml',
     ],
 
