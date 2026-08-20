36c129b26860cffdc13a407e9a606ac2ffda4082
phyoeminko <devphyoeminko@gmail.com>
2026-08-20 20:30:05 +0630

    [UPDATE] purchase order line

---
+++ purchase_internal/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Purchase Internal",
+    "version": "18.0.1.0.0",
+    "summary": "Show internal product information on purchase order lines",
+    "category": "Purchases",
+    "author": "dev_pmk",
+    "license": "LGPL-3",
+    "depends": ["purchase", "product_internal"],
+    "data": [
+        "views/purchase_order_line_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
